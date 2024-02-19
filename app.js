34c46aa8eabb955d92bee4c88107d042f0fd3295
GeorgiGenkov <georgi.genkov.email@gmail.com>
2024-02-19 18:11:14 +0200

    UI upgrades

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// API key and URL
 const apiKey = "bc16d6c8e2bccc4383d2228f4c6305e1";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherInfo = document.querySelector(".weather");
+const errorInfo = document.querySelector(".error-msg");
 
 
 async function checkWeather(city){
@@ -14,24 +17,32 @@ async function checkWeather(city){
     const windSpeed = document.querySelector(".wind");
     const weatherIcon = document.querySelector(".weatherIcon");
 
+    // Error message is shown to the user if their input is a non-existing city name
     if(response.status == 404){
-        const message = `An error has occured: ${response.status}`;
-        cityField.textContent = "City name not found";
+        const message = `An error has occurred: ${response.status}`;
+        cityField.textContent = "City";
         temperature.textContent = "°c";
         humidity.textContent = "%";
         windSpeed.textContent = "km/h";
         weatherIcon.src = "./img/Clear.png";
-        
+
+        weatherInfo.style.display = "none";
+        errorInfo.style.display = "block";
+
         throw new Error(message);
     }
+    // If user's input is an existing city name - weather details are displayed
     else{
-        var data = await response.json();
+        let data = await response.json();
 
         cityField.textContent= data.name;
         temperature.textContent = `${Math.round(data.main.temp)}°c`;
         humidity.textContent = `${data.main.humidity} %`;
         windSpeed.textContent = `${data.wind.speed.toFixed(1)} km/h`;
         weatherIcon.src = `./img/${data.weather[0].main}.png`;
+
+        weatherInfo.style.display = "block";
+        errorInfo.style.display = "none";
     }   
 }
 
